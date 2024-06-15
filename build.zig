9f76da03cde4da2bc077dd82aa3885f27b6e48c3
nilnesil <2021270083@email.szu.edu.cn>
2024-06-15 13:05:21 +0800

    fix

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule("svd2zig-core", .{
+    _ = b.addModule("svd2zig-core", .{
         .root_source_file = b.path("src/zig-generator.zig"),
     });
 }
