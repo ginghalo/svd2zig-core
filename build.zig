5dd22679bcbb6571145adc26e25cb5c10fdeb6c3
nilnesil <2021270083@email.szu.edu.cn>
2024-06-15 13:28:36 +0800

    build.zig

--- build.zig
+++ build.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     _ = b.addModule("svd2zig-core", .{
         .root_source_file = b.path("src/zig-generator.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 }
